7626b0116b8872cf6fd7c4f4eaf4e7476aa4ec76
rabius-sunny <rabiussunnyc3x@gmail.com>
2024-03-02 19:58:56 +0600

    chore: cta color

--- components/home/cta.tsx
+++ components/home/cta.tsx
@@ -1,10 +1,10 @@
 export default function Cta({ title }: { title: string }) {
   return (
-    <div className='bg-primary flex flex-col items-center justify-between gap-8 rounded-lg px-4 py-10 md:flex-row'>
+    <div className='bg-secondary shadow-secondary/50 flex flex-col items-center justify-between gap-8 rounded-lg px-4 py-10 shadow-lg md:flex-row'>
       <h2 className='flex-1 text-center text-lg font-medium text-white md:text-start md:text-2xl'>
         {title}
       </h2>
-      <button className='btn text-primary w-full bg-white md:w-auto'>
+      <button className='btn text-primary w-full bg-white shadow-black/20 md:w-auto'>
         join now
       </button>
     </div>
