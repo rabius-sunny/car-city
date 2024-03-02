ace02f41891c89128ad6ddf066ffa1ba45198a03
rabius-sunny <rabiussunnyc3x@gmail.com>
2024-03-02 12:47:28 +0600

    update: features layout

--- components/home/features.tsx
+++ components/home/features.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 import { featuresData } from '~/constants/features'
 import { CheckCircleIcon } from 'lucide-react'
 
@@ -7,31 +8,59 @@ import SectionHeader from '../shared/section-header'
 export default function Features() {
   return (
     <div>
-      <SectionHeader
-        className='mt-0'
-        title='effortless car care'
-        header='Get the Services You Need, Delivered by AI'
-      />
-      <Container size='md' className='grid grid-cols-1 gap-4 md:grid-cols-2'>
-        {featuresData.user.map((item, idx) => (
-          <div className='col-auto flex items-start gap-4' key={idx}>
-            <CheckCircleIcon className='size-8 text-green-500' />
-            <p className='font-medium'>{item}</p>
+      <Container className='grid grid-cols-1 items-center gap-4 md:grid-cols-2'>
+        <div>
+          <Image
+            src='/user.jpeg'
+            alt='user benifits'
+            width={400}
+            height={300}
+            className='w-full'
+          />
+        </div>
+        <div>
+          <h2 className='font-medium uppercase text-sky-500'>
+            effortless car care
+          </h2>
+          <h1 className='text-2xl font-semibold text-slate-700 sm:text-3xl'>
+            Get the Services You Need, Delivered by AI
+          </h1>
+          <div className='mt-6 grid gap-4'>
+            {featuresData.user.map((item, idx) => (
+              <div className='flex items-start gap-4' key={idx}>
+                <CheckCircleIcon className='size-8 text-green-500' />
+                <p className='font-medium'>{item}</p>
+              </div>
+            ))}
           </div>
-        ))}
+        </div>
       </Container>
-      <SectionHeader
-        className='mt-10'
-        title='The Future of Car Services'
-        header="Partner with Car City's AI-Powered Platform and Thrive"
-      />
-      <Container size='md' className='grid grid-cols-1 gap-4 md:grid-cols-2'>
-        {featuresData.business.map((item, idx) => (
-          <div className='col-auto flex items-start gap-4' key={idx}>
-            <CheckCircleIcon className='size-8 text-green-500' />
-            <p className='font-medium'>{item}</p>
+      <Container className='mt-10 grid grid-cols-1 items-center gap-4 md:grid-cols-2'>
+        <div className='md:order-last'>
+          <Image
+            src='/business.jpeg'
+            alt='business benifits'
+            width={400}
+            height={300}
+            className='w-full'
+          />
+        </div>
+        <div>
+          <h2 className='font-medium uppercase text-sky-500'>
+            The Future of Car Services
+          </h2>
+          <h1 className='text-2xl font-semibold text-slate-700 sm:text-3xl'>
+            Partner with Car City's AI-Powered Platform and Thrive
+          </h1>
+          <div className='mt-6 grid gap-4'>
+            {featuresData.business.map((item, idx) => (
+              <div className='flex items-start gap-4' key={idx}>
+                <CheckCircleIcon className='size-8 text-green-500' />
+                <p className='font-medium'>{item}</p>
+              </div>
+            ))}
           </div>
-        ))}
+        </div>
       </Container>
     </div>
   )
