3838313bc5ac4c28def67cb1dced1225f5293938
rabius-sunny <rabiussunnyc3x@gmail.com>
2024-03-02 19:44:47 +0600

    add: not found

---
+++ app/not-found.tsx
@@ -0,0 +1,12 @@
+export default function Notfound() {
+  return (
+    <div className='flex h-[60vh] items-center justify-center lg:h-[70vh]'>
+      <div className='text-center'>
+        <h1 className='text-8xl font-black text-rose-500'>4O4</h1>
+        <p className='mt-2 font-medium text-slate-500'>
+          Sorry, the page you're searching for isn't created yet.
+        </p>
+      </div>
+    </div>
+  )
+}
