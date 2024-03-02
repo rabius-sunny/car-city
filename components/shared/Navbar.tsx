4bcec7a711d1f004c99f03831d165fdb1d607796
rabius-sunny <rabiussunnyc3x@gmail.com>
2024-03-02 13:41:52 +0600

    update: navbar

--- components/shared/Navbar.tsx
+++ components/shared/Navbar.tsx
@@ -1,21 +1,34 @@
+'use client'
+
 import Image from 'next/image'
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+import { navitems } from '~/constants/info'
+import { cn } from '~/lib/utils'
 
 import Container from './container'
 
 export default function Navbar() {
+  const pathname = usePathname()
   return (
     <Container className='z-10 flex flex-col items-center justify-between text-white md:flex-row'>
       <Link href='/'>
         <Image src='/logo.png' alt='car city logo' width={80} height={80} />
       </Link>
-      <div className='flex items-center gap-4 text-xs font-medium uppercase sm:text-sm lg:gap-8'>
-        <h2>Services</h2>
-        <h2>Rent a car</h2>
-        <h2>Garages</h2>
-        <h2>Exchage</h2>
-        <h2>About us</h2>
-        <h2>Contact</h2>
+      <div className='flex items-center gap-2 text-xs font-medium uppercase sm:gap-4 sm:text-sm lg:gap-8'>
+        {navitems.map((item, idx) => (
+          <Link
+            href={item.href}
+            key={idx}
+            className={cn(
+              'hover:text-sky-500',
+              pathname === item.href &&
+                'text-sky-500 underline underline-offset-2'
+            )}
+          >
+            {item.name}
+          </Link>
+        ))}
       </div>
     </Container>
   )
---
+++ constants/info.ts
@@ -0,0 +1,14 @@
+type TNavitem = {
+  name: string
+  href: string
+}
+
+export const navitems: TNavitem[] = [
+  { name: 'home', href: '/' },
+  { name: 'service', href: '/service' },
+  { name: 'rent a car', href: '/rent-a-car' },
+  { name: 'garages', href: '/garages' },
+  { name: 'exchange', href: '/exchange' },
+  { name: 'about us', href: '/about-us' },
+  { name: 'contact', href: '/contact' }
+]
